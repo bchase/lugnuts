36480f4492f49a9addee0f8b1607b426d2468cdf
Ryan Devlin <ryandevlin86@gmail.com>
2017-10-31 23:59:24 -0400

    Adds Missing Image and Implement Association
    Added a default image for when a product doesn't have an image
    Implemented the Rails association for Product and Productline

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,4 @@
 class Product < ApplicationRecord
+  self.primary_key = "productCode"
+  belongs_to :productlines, class_name: 'Productline', foreign_key: 'productLine'
 end
--- app/models/productline.rb
+++ app/models/productline.rb
@@ -1,2 +1,4 @@
 class Productline < ApplicationRecord
+  self.primary_key = "productLine"
+  has_many :products, foreign_key: 'productLine'
 end
